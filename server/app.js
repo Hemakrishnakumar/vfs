46d9d739244f2828a937e99789c9dd7c880d2066
Hemakrishna <jayavarapukrishna211@gmail.com>
2025-04-03 22:53:03 +0530

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import directoryRoutes from "./routes/directoryRoutes.js";
+import fileRoutes from "./routes/fileRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/directory", directoryRoutes);
+app.use("/file", fileRoutes);
+app.use("/user", userRoutes);
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: "Something went wrong!" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
